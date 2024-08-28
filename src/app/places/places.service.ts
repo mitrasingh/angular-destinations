eda6d9cba8e64f50572fcf43fa9aa86898386a52
mitrasingh <mitra.coder@gmail.com>
2024-08-28 09:09:23 -0400

    add: fetchPlaces method to return data endpoint which accepts url and errorMessage as parameters

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,11 +1,13 @@
-import { Injectable, signal } from '@angular/core';
-
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 import { Place } from './place.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
+  private httpClient = inject(HttpClient);
   private userPlaces = signal<Place[]>([]);
 
   loadedUserPlaces = this.userPlaces.asReadonly();
@@ -17,4 +19,14 @@ export class PlacesService {
   addPlaceToUserPlaces(place: Place) {}
 
   removeUserPlace(place: Place) {}
+
+  private fetchPlaces(url: string, errorMessage: string) {
+    return this.httpClient.get<{ places: Place[] }>(url).pipe(
+      map((res) => res.places),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error(errorMessage));
+      })
+    );
+  }
 }
