636d050fa530abef50d39fae29af21794f1b8664
mitrasingh <mitra.coder@gmail.com>
2024-08-30 18:19:11 -0400

    update: remove next function in observer object

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -23,9 +23,6 @@ export class UserPlacesComponent implements OnInit {
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (places) => {
-        this.places.set(places);
-      },
       error: (error: Error) => {
         this.error.set(error.message);
       },
